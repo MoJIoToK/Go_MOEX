dcd3a127c1176b9926154e60c66849e3c5f1ae4a
Nick <mojiotok167@gmail.com>
2023-09-28 18:24:22 +0300

    Add template

---
+++ get.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"unicode"
+)
+
+func main() {
+	resp, err := http.Get("https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR/securities/SBER.csv?iss.only=securities&securities.columns=PREVPRICE")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+	str := ""
+	for true {
+
+		bs := make([]byte, 1014)
+		n, err := resp.Body.Read(bs)
+		rs := bytes.Runes(bs)
+
+		for i := 0; i < len(rs); i++ {
+			if unicode.IsDigit(rs[i]) || unicode.IsPunct(rs[i]) {
+				str = str + string(rs[i])
+				//fmt.Println(string(rs[:n]))
+			}
+		}
+
+		if n == 0 || err != nil {
+			break
+		}
+	}
+	fmt.Println(str)
+}
